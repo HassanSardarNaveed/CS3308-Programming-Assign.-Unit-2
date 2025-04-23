a8b043c86f4038f47629fc923964b93c7be95d68
Hassan Sardar Naveed <hassannaveed@my.uopeople.edu>
2025-04-24 02:27:11 +0500

    Add files via upload

--- indexer.py
+++ indexer.py
@@ -1,36 +1,52 @@
+#  Example code in python programming language demonstrating some of the features of an inverted index.
+#  In this example, we scan a directory containing the corpus of files. (In this case the documents are reports on articles
+#  and authors submitted to the Journal "Communications of the Association for Computing Machinery"  
+#
+#  In this example we see each file being read, tokenized (each word or term is extracted) combined into a sorted 
+#  list of unique terms.  
+#
+#  We also see the creation of a documents dictionary containing each document in sorted form with an index assigned to it.
+#  Each unique term is written out into a terms dictionary in sorted order with an index number assigned for each term.  
+#  From our readings we know that to complete teh inverted index all that we need to do is create a third file that will
+#  coorelate each term with the list of documents that it was extracted from.  We will do that in a later assignment.
+##
+#  We can further develop this example by keeping a reference for each term of the documents that it came from and by 
+#  developing a list of the documents thus creating the term and document dictionaries. 
+#
+#  As you work with this example, think about how you might enhance it to assign a unique index number to each term and to 
+#  each document and how you might create a data structure that links the term index with the document index. 
+
+
 import os
 import time
 
-# Global counters
+# Define global counters
 tokens = 0
 documents = 0
 terms = 0
 termindex = 0
-docindex = 0 
+docindex = 0
 
-# Lists to store data
+# Initialize lists
 alltokens = []
 alldocs = []
 
-# Start time
-start_time = time.localtime()   
+# Capture start time
+t2 = time.localtime()
 
-# === TO BE EDITED IF REQUIRED ===
-# Set the directory containing the corpus files
-dirname = r"C:\Assignment\cacm"  # Use raw string (r"") to avoid escape characters on Windows
+# Set directory path
+dirname = r"C:\Users\croom\Downloads\Compressed\New folder (3)\cacm"
 
 # Read and tokenize each document in the directory
-all = [f for f in os.listdir(dirname) if os.path.isfile(os.path.join(dirname, f))]
-
-for f in all:
+all_files = [f for f in os.listdir(dirname)]
+for f in all_files:
     documents += 1
     with open(os.path.join(dirname, f), 'r', encoding='utf-8') as myfile:
         alldocs.append(f)
-        data = myfile.read().replace('\n', ' ')
-        alltokens.extend(data.split())
-
-# Count total tokens
-tokens = len(alltokens)
+        data = myfile.read().replace('\n', '')
+        for token in data.split():
+            alltokens.append(token)
+            tokens += 1
 
 # Write document dictionary to file
 documentfile = open(os.path.join(dirname, 'documents.dat'), 'w', encoding='utf-8')
@@ -40,24 +56,28 @@ for f in alldocs:
     documentfile.write(f + ',' + str(docindex) + os.linesep)
 documentfile.close()
 
-# Sort all tokens and identify unique ones
+# Sort tokens
 alltokens.sort()
-unique_terms = sorted(set(alltokens))
-terms = len(unique_terms)
 
-# Write term dictionary (index.dat) to file
+# Identify unique terms
+g = []
+for i in alltokens:
+    if i not in g:
+        g.append(i)
+        terms += 1
+
+# Write term index to file
 indexfile = open(os.path.join(dirname, 'index.dat'), 'w', encoding='utf-8')
-for i in unique_terms:
+for i in g:
     termindex += 1
     indexfile.write(i + ',' + str(termindex) + os.linesep)
 indexfile.close()
 
-# End time
-end_time = time.localtime()
-
-# Output statistics
-print('Processing Start Time: {:02d}:{:02d}'.format(start_time.tm_hour, start_time.tm_min))
+# Output metrics
+print('Processing Start Time: {:02d}:{:02d}'.format(t2.tm_hour, t2.tm_min))
 print("Documents:", documents)
 print("Tokens:", tokens)
 print("Terms:", terms)
-print('Processing End Time: {:02d}:{:02d}'.format(end_time.tm_hour, end_time.tm_min))
+
+t2 = time.localtime()
+print('Processing End Time: {:02d}:{:02d}'.format(t2.tm_hour, t2.tm_min))
